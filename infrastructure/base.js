4f80529a1792dc01d563522052ce4f816c6e849e
Tonje <tonje.sundby@schibsted.no>
2016-03-08 15:31:23 +0100

    test

--- infrastructure/base.js
+++ infrastructure/base.js
@@ -1,17 +1,17 @@
 var baseConfig = {
-    name: 'tonjes-book-inventory-test',
     region: 'eu',
     maintenance: false,
     stack: 'cedar-14',
     addons: {mongolab: {plan: 'mongolab:sandbox'}},
-    collaborators: [ 'tonje.sundby@schibsted.no', 'yvonne.richa@schibsted.no' ],
+    collaborators: ['tonje.sundby@schibsted.no', 'yvonne.richa@schibsted.no'],
     features: {
         'runtime-dyno-metadata': {enabled: false},
-        'log-runtime-metrics': {enabled: false},
+        'log-runtime-metrics': {enabled: true},
         'http-session-affinity': {enabled: false},
         preboot: {enabled: false},
         'http-shard-header': {enabled: false},
-        'http-end-to-end-continue': {enabled: false}
+        'http-end-to-end-continue': {enabled: false},
+        'runtime-px-upgrade': {enabled: true}
     },
     formation: [{process: 'web', quantity: 1, size: 'Free'}],
     log_drains: []
