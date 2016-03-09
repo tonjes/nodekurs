38af226f570f333e83c64e333a99578d387faeda
Tonje <tonje.sundby@schibsted.no>
2016-03-09 12:19:01 +0100

    test

--- routes.js
+++ routes.js
@@ -1,11 +1,30 @@
 module.exports = function(stockRepository) {
     return {
         getCount: function (req, res) {
+            console.log("get count isbn", req.params.isbn);
             stockRepository.getCount(req.params.isbn).then(function (result) {
+                console.log("get result", result);
                 if (result !== null) {
-                    res.status(200).json({count: result});
+                    res.format({
+                        html: function(){
+                            res.send('<div class="available">Available</div>');
+                        },
+                        json: function(){
+                            res.send({ status: 'available' });
+                        }
+                    });
+                    //res.status(200).json({count: result});
                 } else {
-                    res.status(404).json({error: 'No book with ISBN: ' + req.params.isbn});
+                    res.format({
+                        html: function(){
+                            res.send('<div class="notAvailable">Not available</div>');
+                        },
+
+                        json: function(){
+                            res.send({ status: 'notAvailable' });
+                        }
+                    });
+                    //res.status(404).json({error: 'No book with ISBN: ' + req.params.isbn});
                 }
             });
         },
--- stockRepository.js
+++ stockRepository.js
@@ -1,25 +1,25 @@
 var url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/mongo_books_oslo_1';
 var MongoClient = require('mongodb').MongoClient;
-var connection = MongoClient.connect(url);
+var collection = MongoClient.connect(url).then(function (db) { return db.collection('books'); });
 
 module.exports = function () {
     return {
         findAll: function () {
-            return connection.then(function (db) {
-                return db.collection('books').find({}).toArray();
+            return collection.then(function (books) {
+                return books.find({}).toArray();
             });
         },
         stockUp: function (isbn, count) {
-            return connection.then(function (db) {
-                return db.collection('books').updateOne({isbn: isbn}, {
+            return collection.then(function (books) {
+                return books.updateOne({isbn: Number(isbn)}, {
                     isbn: isbn,
                     count: count
                 }, {upsert: true});
             });
         },
         getCount: function (isbn) {
-            return connection.then(function (db) {
-                return db.collection('books').find({"isbn": isbn}).limit(1).next();
+            return collection.then(function (books) {
+                return books.find({"isbn": Number(isbn)}).limit(1).next();
             }).then(function (result) {
                 if (result) {
                     return result.count;
